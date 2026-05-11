9d27fb14a9f97c56cc40fd23bba1e8a9a5269af1
Aman Manthira <amanmanthira32326@gmail.com>
2026-05-11 12:08:54 +0530

    APL Call erros fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SubCategoriesModule } from './sub-categories/sub-categories.module';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, CategoriesModule, SubCategoriesModule, ItemsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    CategoriesModule,
+    SubCategoriesModule,
+    ItemsModule,
+  ],
 })
 export class AppModule {}
