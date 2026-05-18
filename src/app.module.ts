6f8dfcbe587af00ec22c86b409615a47275cf12f
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2026-05-18 12:41:33 +0530

    Remove item

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SubCategoriesModule } from './sub-categories/sub-categories.module';
-import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { GoodsReceivedNotesModule } from './goods-received-notes/goods-received-notes.module';
 import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
@@ -16,7 +15,6 @@ import { GoodsIssueNotesModule } from './goods-issue-notes/goods-issue-notes.mod
     PrismaModule,
     CategoriesModule,
     SubCategoriesModule,
-    ItemsModule,
     GoodsReceivedNotesModule,
     PurchaseOrdersModule,
     GoodsIssueNotesModule,
